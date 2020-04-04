1cccc52832b23d2ea8ac0b640387713e67618951
caioqazz <caioqazz@gmail.com>
2020-04-04 17:56:58 -0300

    Add form validation

--- StardewValleyFishes.Core/Fish.cs
+++ StardewValleyFishes.Core/Fish.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace StardewValleyFishes.Core
 {
@@ -10,8 +11,13 @@ namespace StardewValleyFishes.Core
     {
 
         public int Id { get; set; }
+        
+        [Required, StringLength(80)]
         public string Name { get; set; }
+
+        [Required, StringLength(80)]
         public string Description { get; set; }
+
         public SeasonType Season { get; set; }
         public Location Local { get; set; }
         public WeatherType Weather { get; set; }
