cfb2ca0df49915fbf823800f0e1f0e9da6bf22da
Caio Cesar <caioqazzz@hotmail.com>
2020-04-04 13:49:21 -0300

    Add detail page and notfound

---
+++ StardewValleyFishes/Pages/Fishes/Detail.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StardewValleyFishes.Core;
+using StardewValleyFishes.Data;
+
+namespace StardewValleyFishes.Pages.Fishes
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IFishData fishData;
+
+        public Fish Fish { get; set; }
+
+        public DetailModel(IFishData fishData)
+        {
+            this.fishData = fishData;
+        }
+
+        public IActionResult OnGet(int fishId)
+        {
+            Fish = fishData.GetById(fishId);
+            if (Fish == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
